de8083d61d4a0393225d0ccd45b8ca69ce36eb25
taratokikawa <taratokikawa@gmail.com>
2026-01-30 11:22:46 -0800

    homepage layout

--- frontend/SoundWave/website/App.tsx
+++ frontend/SoundWave/website/App.tsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { FC } from 'react';
+import Header from '../components/Header';
 // homepage
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>SoundWave Homepage</Text>
-      <StatusBar style="auto" />
+      <Header title="SoundWave"/>
+
+      <Text style={styles.title}>Popular</Text>
+      <View style={styles.rowContainer}>
+      </View>
+
+      <Text style={styles.title}>New Releases</Text>
+
     </View>
   );
 }
@@ -16,6 +23,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  rowContainer: {
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginTop: 20,
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+  },  
 });
