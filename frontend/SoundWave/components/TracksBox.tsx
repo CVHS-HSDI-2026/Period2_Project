5e44e5c08fef12da52b0b03aa84ca2d3887b763b
Catherine Chiu <catkaiting@gmail.com>
2026-02-27 09:39:36 -0800

    added runtime and ratings on track page

--- frontend/SoundWave/components/TracksBox.tsx
+++ frontend/SoundWave/components/TracksBox.tsx
@@ -20,23 +20,61 @@ export default function TracksBox() {
     "9. Count On Me",
     "10. The Other Side (ft. Cee Lo Green & B.o.B)",
   ];
-
+  const runTime =[
+    "3:42",
+    "4:02",
+    "3:55", 
+    "3:15",
+    "3:09",
+    "3:50",
+    "4:17",
+    "4:05",
+    "4:12",
+    "3:45",
+  ]
+  const trackRatings = [
+    "8",
+    "9",
+    "7.5",
+    "8.5",
+    "7",
+    "9",  
+    "8.5",
+    "7.5",
+    "8",
+    "9",
+  ]
   return (
     <ScrollView
       style={styles.scrollContainer}
       contentContainerStyle={styles.scrollContent}
       showsVerticalScrollIndicator={false}
     >
-      {tracks.map((track, index) => (
-        <TouchableOpacity key={index} onPress={() => router.push("Song")}>
-          <Text style={styles.commentText}>{track}</Text>
-        </TouchableOpacity>
-      ))}
-
+      <View style={styles.row}>
+        <View style={[styles.column, {alignItems: 'flex-start'}, {width: "78%"},{paddingLeft:10}]}>
+          {tracks.map((track, index) => (
+            <TouchableOpacity key={index} onPress={() => router.push("Song")}>
+              <Text style={styles.commentText}>{track}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={[styles.column, {alignItems: 'flex-start'}, {width: "12%"}]}>
+          {runTime.map((time, index) => (
+            <Text style={styles.commentText}>{time}</Text>
+          ))}
+        </View>
+        <View style={[styles.column, {alignItems: 'flex-start'}, {width: "10%"}, {paddingRight:10}]}>
+          {trackRatings.map((rating, index) => (
+            <Text style={styles.commentText}>{rating}/10</Text>
+          ))}
+        </View>
+      </View>
       {/* Optional placeholder for when no tracks are available */}
       {tracks.length === 0 && (
         <Text style={styles.emptyText}>Tracks unavailable.</Text>
       )}
+      
+
     </ScrollView>
   );
 }
@@ -60,4 +98,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 10,
   },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  column:{
+    flexDirection: "column",
+    justifyContent: "space-between",
+  }
 });
