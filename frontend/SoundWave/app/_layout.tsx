b09f04eb37ba0bd6a1ef5a1667a78a8ed4ba856d
SJ-blue <sjbolen42@gmail.com>
2026-03-16 19:59:28 -0700

    Add Signup screen to navigation layout

--- frontend/SoundWave/app/_layout.tsx
+++ frontend/SoundWave/app/_layout.tsx
@@ -10,6 +10,7 @@ export default function Layout() {
       <Stack.Screen name="Song" options={{ title: 'Song' }} />
       <Stack.Screen name="Album" options={{ title: 'Album' }} />
       <Stack.Screen name="Login" options={{ title: 'Login' }} />
+      <Stack.Screen name="Signup" options={{ title: 'Signup' }} />
       <Stack.Screen name="Profile" options={{ title: 'Profile' }} />
       <Stack.Screen name="Settings" options={{ title: 'Settings' }} />
       <Stack.Screen name="Homepage" options={{ title: 'Homepage' }} />
